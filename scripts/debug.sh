77a850a0937c516d2fe518e91f87cdb61fe7b420
Vu Tran <baysao@gmail.com>
2022-03-31 12:40:28 +0700

    Hotfix: Clean up run script

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -7,6 +7,7 @@ source $SITE_ROOT/scripts/base.sh
 _load_env $SITE_ROOT
 
 _send_log() {
+	debug_log=$SITE_ROOT/logs/debug.log
 	id=$(cat $SITE_ROOT/vars/ID)
 	# curl -X POST https://monitor.mbr.${DOMAIN}/upload/gateway/${id}_monitor_client.log --data-binary @$log_dir/monitor_client.log
 	timeout 5 curl -X POST https://monitor.mbr.${DOMAIN}/upload/gateway/$id --data-binary @$debug_log
