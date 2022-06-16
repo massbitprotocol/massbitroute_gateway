d3fcdb27c4089e451a27a7a2ca16c0ff5453e5d8
Vu Tran <baysao@gmail.com>
2022-06-16 14:44:35 +0700

    Add fisherman service

--- services/fisherman/run.sh
+++ services/fisherman/run.sh
@@ -4,12 +4,13 @@ ROOT=$(realpath $(dirname $(realpath $0))/../..)
 VERSION=
 source $ROOT/.env_raw
 
+TYPE=gw
 export RUST_LOG=debug
 export SCHEDULER_ENDPOINT=https://scheduler.fisherman.$DOMAIN
 export WORKER_ID=$(cat $ROOT/vars/ID)
 export WORKER_IP=$(cat $ROOT/vars/IP)
-export WORKER_ENDPOINT=https://$WORKER_ID.gw.mbr.$DOMAIN/__worker
-export WORKER_SERVICE_ENDPOINT=0.0.0.0:4040
+export WORKER_ENDPOINT=https://$WORKER_ID.${TYPE}.mbr.$DOMAIN/__worker
+export WORKER_SERVICE_ENDPOINT=127.0.0.1:4040
 export BENCHMARK_WRK_PATH=benchmark
 
 $ROOT/services/fisherman/fisherman$VERSION
